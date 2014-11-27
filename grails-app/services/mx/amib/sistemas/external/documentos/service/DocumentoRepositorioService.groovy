59aa6f1f69ab3c7e09ef1b8a9e259fa6756c96fd
gabriel-medina <gabriel.m.g.245245@gmail.com>
2014-11-27 15:24:50 -0600

    DEV
    desc: se actualizan servicos para documentos

--- grails-app/services/mx/amib/sistemas/external/documentos/service/DocumentoRepositorioService.groovy
+++ grails-app/services/mx/amib/sistemas/external/documentos/service/DocumentoRepositorioService.groovy
@@ -1,17 +1,24 @@
 package mx.amib.sistemas.external.documentos.service
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.Date
 
 import mx.amib.sistemas.util.service.ArchivoTO
 import mx.amib.sistemas.util.service.ArchivoTemporalService
+import mx.amib.sistemas.utils.AmibFechaUtils
 
 import org.codehaus.groovy.grails.web.json.JSONObject
+import org.junit.After;
+import org.springframework.aop.aspectj.RuntimeTestWalker.ThisInstanceOfResidueTestVisitor;
 
 import java.io.File
 
 import grails.converters.JSON
 import grails.plugins.rest.client.RestBuilder
 import grails.transaction.Transactional
+
+
 
 /**
  * DocumentoRepositorioService
@@ -20,7 +27,7 @@ import grails.transaction.Transactional
  * HTTP/REST al sistema de amibDocumentos
  *
  * @author Gabriel
- * @version 1.0 - (Última actualización) 12/09/2014
+ * @version 1.1 - (Última actualización) 22/09/2014
  *
  */
 @Transactional
@@ -30,20 +37,96 @@ class DocumentoRepositorioService {
 	
 	String saveUrl
 	String documentoPoderSaveUrl
+	String documentoPoderUpdateUrl
+	String documentoRevocacionSaveUrl
+	String documentoRevocacionUpdateUrl
+	
 	String saveMultipartUrl
 	String getUrl
 	String downloadUrl
 	String deleteUrl
 	
-    void enviarDocumentosArchivoTemporal(Collection<DocumentoRepositorioTO> docs) {
+	/**
+	 * Obtiene, del repositorio amibDocumentos,
+	 * los metadatos de un documento dado su UUID
+	 *
+	 * @param uuid
+	 * @return Instanca de DocumentoRepositorioTO
+	 */
+	DocumentoRepositorioTO obtenerMetadatosDocumento(String uuid, ClaseDocumento cd = ClaseDocumento.DOCUMENTO){
+		def docRep = null
+		String restUrl = getUrl + uuid
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd")
+		
+		def rest = new RestBuilder()
+		def restMultipart = new RestBuilder()
+		
+		def resp = rest.get(restUrl)
+		resp.json instanceof JSONObject
+		
+		if(resp.json == null)
+			return null
+		else
+		{
+			switch(cd){
+				case ClaseDocumento.DOCUMENTO:
+					docRep = new DocumentoRepositorioTO()
+				break;
+				case ClaseDocumento.OFICIO_CNBV:
+					docRep = new DocumentoOficioCnbvRespositorioTO()
+					docRep.datosAdicionales = resp.json.'datosAdicionales'
+				break;
+				case ClaseDocumento.PODER:
+					docRep = new DocumentoPoderRepositorioTO()
+					docRep.tipoDocumentoRespaldo = resp.json.'tipoDocumentoRespaldo'
+					docRep.representanteLegalNombre = resp.json.'representanteLegalNombre'
+					docRep.representanteLegalApellido1 = resp.json.'representanteLegalApellido1'
+					docRep.representanteLegalApellido2 = resp.json.'representanteLegalApellido2'
+					docRep.esRegistradoPorGrupoFinanciero = resp.json.'esRegistradoPorGrupoFinanciero'
+					docRep.numeroEscritura = resp.json.'numeroEscritura'
+					docRep.fechaApoderamiento = df.parse(resp.json.'fechaApoderamiento'.substring(0,10))
+					docRep.jsonApoderados = resp.json.'jsonApoderados'
+					docRep.jsonNotario = resp.json.'jsonNotario'
+					docRep.jsonGrupoFinanciero = resp.json.'jsonGrupoFinanciero'
+					docRep.jsonInstitucion = resp.json.'jsonInstitucion'
+				break;
+				case ClaseDocumento.FOTO_SUSTENTANTE:
+					docRep = new DocumentoFotoSustentanteRepositorioTO()
+					docRep.datosAdicionales = resp.json.'datosAdicionales'
+				break;
+				case ClaseDocumento.DOC_SUSTENTANTE:
+					docRep = new DocumentoSustentanteRepositorioTO()
+					docRep.datosAdicionales = resp.json.'datosAdicionales'
+				break;
+			}
+			docRep.id = resp.json.'id'
+			docRep.uuid = resp.json.'uuid'
+			docRep.clave = resp.json.'clave'
+			docRep.nombre = resp.json.'nombre'
+			docRep.mimetype = resp.json.'mimetype'
+			//docRep.fechaModificacion = resp.json.'fechaModificacion'
+			//docRep.fechaCreacion = resp.json.'fechaCreacion'
+		}
+		return docRep
+	}
+	
+	/**
+	 * Envía los documentos indicados al repositorio amibDocumentos,
+	 * siempre y cuando su archivo correspondiente se encuentre
+	 * almacenado en temporal (usando el servicio ArchivoTemporalService)
+	 *
+	 * @param docs
+	 */
+	void enviarDocumentosArchivoTemporal(Collection<DocumentoRepositorioTO> docs) {
+
 		docs.each{
 			String restUrl = null
 			
 			it.id = null //este siempre se debe setear nulo para que pueda guardar el nuevo documento
 			it.nombre = archivoTemporalService.obtenerArchivoTemporal(it.uuid).filename
 			it.mimetype = archivoTemporalService.obtenerArchivoTemporal(it.uuid).mimetype
-			it.fechaModificacion = new Date()
-			it.fechaCreacion = new Date()
+			it.fechaModificacion = AmibFechaUtils.obtenerFechaZ()
+			it.fechaCreacion = AmibFechaUtils.obtenerFechaZ()
 			
 			def rest = new RestBuilder()
 			def restMultipart = new RestBuilder()
@@ -52,6 +135,9 @@ class DocumentoRepositorioService {
 			
 			if( DocumentoPoderRepositorioTO.class.isInstance(it) ){
 				restUrl = this.documentoPoderSaveUrl
+			}
+			else if ( DocumentoRevocacionRepositorioTO.class.isInstance(it) ){
+				restUrl = this.documentoRevocacionSaveUrl
 			}
 			
 			//Envía acorde al metadato
@@ -75,18 +161,101 @@ class DocumentoRepositorioService {
 
 			archivoTemporalService.eliminarArchivoTemporal(_uuid)
 		}
-    }
+	}
 	
+	void actualizaMetadatosDocumentos(Collection<DocumentoRepositorioTO> docs){
+		docs.each{
+			this.actualizaMetadatosDocumento(it)
+		}
+	}
+	
+	/**
+	 * Actualiza UNICAMENTE los metadatos relativos al documento,
+	 * por lo que no actualizara datos como nombre, mimetype ni clave
+	 *
+	 * @param doc
+	 */
+	void actualizaMetadatosDocumento(DocumentoRepositorioTO doc){
+		String restUrl = null
+		
+		doc.id = null
+		doc.fechaModificacion = new Date()
+		
+		def rest = new RestBuilder()
+		def restMultipart = new RestBuilder()
+		String _uuid = doc.uuid
+		String _json = (doc as JSON)
+		
+		if( DocumentoPoderRepositorioTO.class.isInstance(doc) ){
+			restUrl = this.documentoPoderUpdateUrl
+		}
+		else if ( DocumentoRevocacionRepositorioTO.class.isInstance(doc) ){
+			restUrl = this.documentoRevocacionUpdateUrl
+		}
+		//Envía acorde al metadato
+		def resp = rest.post(restUrl){
+			contentType "application/json;charset=UTF-8"
+			json _json
+		}
+	}
+	
+	/**
+	 * Elimina un documento del repositorio amibDocumentos
+	 * dado su UUID
+	 *
+	 * @param uuid
+	 */
 	void eliminarDocumento(String uuid){
+		String restUrl = deleteUrl + uuid
 		
+		def rest = new RestBuilder()
+		def restMultipart = new RestBuilder()
+		
+		println restUrl
+		def resp = rest.get(restUrl)
+		
+		resp.json instanceof JSONObject
+		
+		if(resp.json.'status' == 'OK'){
+			println uuid + ',Archivo borrado - OK'
+		}
+		else if(resp.json.'status' == 'ERROR'){
+			println resp.json.'status'
+			println resp.json.'details'
+		}
 	}
 	
+	/**
+	 * Elimina multiples documentos del repositorio amibDocumentos
+	 * dado un listado de UUIDs
+	 *
+	 * @param uuids
+	 */
 	void eliminarDocumentos(Collection<String> uuids){
-		
+		uuids.each { uuid ->
+			this.eliminarDocumento(uuid)
+		}
 	}
 	
-	void descargarATemporal(String uuid){
-		
+	/**
+	 * Descarga un documento del repostorio, dado su UUID,
+	 * al almacenamiento temporal.
+	 *
+	 * @param sessionId
+	 * @param uuid
+	 */
+	DocumentoRepositorioTO descargarATemporal(String sessionId, String uuid){
+		//paso 1: obtiene metadatos
+		DocumentoRepositorioTO dr = this.obtenerMetadatosDocumento(uuid)
+		//paso 2: descarga a temporal
+		if(dr != null){
+			//comprueba si ya esta en temporal, si ya esta, solo actualiza caducidad
+			//if(archivoTemporalService.comprobarArchivoTemporal(uuid))
+				//archivoTemporalService.renuevaCaducidadArchivoTemporal(uuid)
+			//else
+				archivoTemporalService.descargarArchivoTemporal(sessionId, uuid, dr.nombre, dr.mimetype, new URL(downloadUrl+uuid))
+		}
+		return dr
 	}
 }
 
@@ -118,10 +287,28 @@ class DocumentoPoderRepositorioTO extends DocumentoRepositorioTO{
 	String jsonInstitucion
 }
 
+class DocumentoRevocacionRepositorioTO extends DocumentoRepositorioTO{
+	String tipoDocumentoRespaldo
+	String representanteLegalNombre
+	String representanteLegalApellido1
+	String representanteLegalApellido2
+	Boolean esRegistradoPorGrupoFinanciero
+	Integer numeroEscritura
+	Date fechaRevocacion
+	String jsonRevocados
+	String jsonNotario
+	String jsonGrupoFinanciero
+	String jsonInstitucion
+}
+
 class DocumentoFotoSustentanteRepositorioTO extends DocumentoRepositorioTO{
 	String datosAdicionales
 }
 
 class DocumentoSustentanteRepositorioTO extends DocumentoRepositorioTO{
 	String datosAdicionales
+}
+
+enum ClaseDocumento{
+	DOCUMENTO, OFICIO_CNBV, PODER, FOTO_SUSTENTANTE, DOC_SUSTENTANTE
 }
--- grails-app/services/mx/amib/sistemas/util/service/ArchivoTemporalService.groovy
+++ grails-app/services/mx/amib/sistemas/util/service/ArchivoTemporalService.groovy
@@ -2,36 +2,59 @@ package mx.amib.sistemas.util.service
 
 import groovy.transform.AutoClone
 import grails.transaction.Transactional
+
 import org.springframework.web.multipart.commons.CommonsMultipartFile
 import org.apache.commons.io.FileUtils
+
 import groovy.time.TimeCategory
+
 import java.util.UUID
 
+import liquibase.util.file.FilenameUtils;
+
 /**
- * ArchivoTemporalService 
- * 
+ * ArchivoTemporalService
+ *
  * Este servicio permite gestionar archivos temporales en la aplicación
- * 
+ *
  * @author Gabriel
- * @version 1.0 - (Última actualización) 12/09/2014
- * 
+ * @version 1.1 - (Última actualización) 16/10/2014
+ *
  */
 @Transactional
 class ArchivoTemporalService {
 
 	private static TreeMap<String,ArchivoTO> archivosTemporales = new TreeMap<String,ArchivoTO>()
 	
-	String directorioTemporal //se inyecta desde spring
+	String directorioTemporal
 	Integer minutosCaducidadPorArchivo
+	
+	/**
+	 * Comprueba si un archivo dado su UUID se
+	 * encuentra almancenado en temporal
+	 *
+	 * @param uuid
+	 * @return
+	 */
+	boolean comprobarArchivoTemporal(String uuid){
+		return this.archivosTemporales.containsKey(uuid)
+	}
+	
+	void renuevaCaducidadArchivoTemporal(String uuid){
+		Date ahorita = new Date()
+		if(this.archivosTemporales.containsKey(uuid)){
+			this.archivosTemporales.get(uuid).caducidad = ahorita + minutosCaducidadPorArchivo.minutes
+		}
+	}
 	
 	/**
 	 * Obtiene los metadatos de un archivo temporal dado su UUID
 	 * siempre cuando halla sido almacenado anteriormente.
-	 * 
+	 *
 	 * @param uuid Identificador universal de archivo
 	 * @return instancia de ArchivoTO con metadatos de archivo
 	 */
-    ArchivoTO obtenerArchivoTemporal(String uuid) {
+	ArchivoTO obtenerArchivoTemporal(String uuid) {
 		ArchivoTO a = null
 		
 		if(this.archivosTemporales.containsKey(uuid)){
@@ -39,16 +62,16 @@ class ArchivoTemporalService {
 		}
 		
 		return a
-    }
+	}
 	
 	/**
 	 * Guarda archivo en temporal; los metadatos son guardados en memoria.
-	 * 
+	 *
 	 * @param sessionId		Identificador de sesión
 	 * @param cmf			Objeto del tipo CommonsMultipartFile que almacena informacion de
 	 * 						archivo recibido por medio de un "multipart"
 	 * @return				Una instancia de ArchivoTO con los metadatos almacenados
-	 * 
+	 *
 	 */
 	ArchivoTO guardarArchivoTemporal(String sessionId, CommonsMultipartFile cmf){
 		ArchivoTO anvo = new ArchivoTO()
@@ -74,8 +97,48 @@ class ArchivoTemporalService {
 	}
 	
 	/**
+	 * Descarga un archivo dada una URL y forma una instancia de ArchivoTO
+	 * la cual estará guardada en temporal.
+	 *
+	 * @param sessionId
+	 * @param uuid
+	 * @param filename
+	 * @param mimetype
+	 * @param source
+	 * @return Una instancia de ArchivoTO con los metadatos almacenados
+	 */
+	ArchivoTO descargarArchivoTemporal(String sessionId, String uuid, String filename, String mimetype, URL source){
+		ArchivoTO anvo = new ArchivoTO()
+		Date ahorita = new Date()
+		
+		anvo.uuid = uuid
+		anvo.sessionId = sessionId
+		anvo.filename = filename
+		anvo.mimetype = mimetype
+		anvo.temploc = directorioTemporal + anvo.uuid
+		use( TimeCategory ) {
+			anvo.caducidad = ahorita + minutosCaducidadPorArchivo.minutes
+		}
+		
+		try{
+			FileUtils.copyURLToFile(source, new File(anvo.temploc) )
+		}
+		catch(IOException e){
+			anvo = null
+		}
+		if(!this.archivosTemporales.containsKey(anvo.uuid)){
+			this.archivosTemporales.put(anvo.uuid, anvo)
+		}
+		else{
+			this.archivosTemporales.remove(anvo.uuid)
+			this.archivosTemporales.put(anvo.uuid, anvo)
+		}
+		return anvo
+	}
+	
+	/**
 	 * Elimina archivo en temporal
-	 * 
+	 *
 	 * @param uuid Identificador universal de archivo
 	 */
 	void eliminarArchivoTemporal(String uuid){
@@ -116,7 +179,7 @@ class ArchivoTemporalService {
 	/**
 	 * Elimina todos los archivos dado un identificador de sesión
 	 * (util para limpiar archivos de sesiones expiradas)
-	 * 
+	 *
 	 * @param sessionId Identificador de session
 	 */
 	void eliminarArchivosTemporalesPorSessionId(String sessionId){
@@ -128,14 +191,14 @@ class ArchivoTemporalService {
 			}
 		}
 		
-		archivosBorrar.each{ archivoBorrar -> 
+		archivosBorrar.each{ archivoBorrar ->
 			this.eliminarArchivoTemporal(archivoBorrar.uuid)
 		}
 	}
 	
 	/**
 	 * Elimina todos los archivos del directorio temporal.
-	 * 
+	 *
 	 * NOTA: Solo se debe llamar al inicio o al final de la aplicación,
 	 * de lo contrario causará comportamiento no esperado.
 	 */
